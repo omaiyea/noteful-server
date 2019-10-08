1d97fe8e94944c65b944381a880cf3cc2a55603d
omaiyea <omaiyea@gmail.com>
2019-10-08 14:47:38 -0700

    add test data

---
+++ seeds/seed_folders.sql
@@ -0,0 +1,5 @@
+INSERT INTO folders (folder_name, date_created)
+VALUES
+ ('Important', '2019-05-01'),
+ ('React Notes', '2019-09-15'),
+ ('Monthly Goals', '2019-07-11');
---
+++ seeds/seed_notes.sql
@@ -0,0 +1,8 @@
+INSERT INTO notes (note_name, content, date_modified, folder_id)
+VALUES
+ ('Go to the doctor', 'Need to do checkup', '2019-05-05', 1),
+ ('Pick up medicine', 'Last day to pick up: 10/9', '2019-09-22', 1),
+ ('Pay bill before Friday', 'Seriously due before Friday', '2019-07-20', 1),
+ ('Context', 'See codecademy for tutorials', '2019-06-05', 2),
+ ('React Router', 'INSERT NOTES', '2019-08-22', 2),
+ ('Do 3 Hacker Ranks', '262, 261, 260', '2019-03-20', 3);
