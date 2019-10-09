2d5aacd5c88333e0e6726ffe1fd78ca51b45289a
omaiyea <omaiyea@gmail.com>
2019-10-08 23:03:38 -0700

    complete service object and router for notes endpoint

--- src/app.js
+++ src/app.js
@@ -10,12 +10,14 @@ const { NODE_ENV } = require('./config');
 const morganOption = (NODE_ENV === 'production' ? 'tiny' : 'common');
 
 const foldersRouter = require('./folders/folders-router');
+const notesRouter = require('./notes/notes-router');
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
 app.use('/api/folders', foldersRouter);
+app.use('/api/notes', notesRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
